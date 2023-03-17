0cdcfa75a6eb0a128cf6c560b973475e1dcc7e72
Behigu Gizachew <47280895+behigu@users.noreply.github.com>
2023-03-17 09:45:01 +0300

    create employee table

---
+++ SQL Scripts/Scripts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `employee_name` varchar(50) NOT NULL,
+  `department` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `salary` float NOT NULL,
+  `age` int(11) NOT NULL,
+  `bonus` float NOT NULL
+)
